2a670e121a6d95deaa5cbbd968643b84d9e4d345
luochp3 <luochp3@yonyou.com>
2017-07-01 21:12:57 +0800

    Add mybatis dao 业务代码

---
+++ src/main/java/com/yonyou/seckill/dao/SecKillDao.java
@@ -0,0 +1,51 @@
+package com.yonyou.seckill.dao;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.yonyou.seckill.entity.SecKill;
+
+/**
+ * 秒杀库存DAO接口
+ * 
+ * @author 李奕锋
+ */
+public interface SecKillDao {
+
+	/**
+	 * 减库存
+	 * 
+	 * @param seckillId
+	 * @param killTime
+	 * @return 如果影响行数等于>1，表示更新的记录行数
+	 */
+	int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
+
+	/**
+	 * 根据id查询秒杀对象
+	 * 
+	 * @param seckillId
+	 * @return
+	 */
+	SecKill queryById(long seckillId);
+
+	/**
+	 * 根据偏移量查询秒杀商品列表
+	 * 
+	 * @param offset
+	 * @param limit
+	 * @return
+	 */
+	List<SecKill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+
+	/**
+	 * 使用存储过程执行秒杀
+	 * 
+	 * @param paramMap
+	 */
+	void killByProcedure(Map<String, Object> paramMap);
+
+}
